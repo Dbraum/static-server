c41f7183c76becc76b7433d4eb4ec31563229932
wukunpeng <757246444@qq.com>
2017-04-28 09:08:24 +0800

    增加路径配置，同时nodemon只需要watch app文件，以及修改静态服务的配置位置

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var proxy = require('http-proxy-middleware');
 var serveIndex = require('serve-index')
-app.use(express.static(__dirname));
+
+//此处设置静态文件的路径
+const basePath = path.resolve('..')
 
 
-app.use('/', serveIndex('.', {'icons': true}))
+app.use(express.static(basePath));
 
 
 // proxy
 //具体配置查看 https://github.com/chimurai/http-proxy-middleware
 app.use('/infinitus-moa-store', proxy({target: 'http://192.168.14.168:8080', changeOrigin: true}));
 
+//需要配置在 proxy配置后面
+app.use('/', serveIndex(basePath, {'icons': true}))
 
 app.listen(3000, function () {
   console.log('app listening on port 3000!')
